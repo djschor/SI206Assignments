e6ced00f6bd73f5da31a830271de79b1b37d99c3
Daniel Schorin <Daniel Schorin>
2017-02-17 16:36:40 -0500

    Second commit today

--- 206W17_HW5.py
+++ 206W17_HW5.py
@@ -5,7 +5,7 @@ import json
 
 ## SI 206 - W17 - HW5
 ## COMMENT WITH:
-## Your section day/time:
+## Your section day/time: Gillian S, Tahmeed 
 ## Any names of people you worked with on this assignment:
 
 ######## 500 points total ########
@@ -14,8 +14,69 @@ import json
 
 ## TEXT: I'm an awesome Python programmer.
 ## CREATED AT: Sat Feb 11 04:28:19 +0000 2017
+consumer_key = "VLljdWFKfyHbBxfOWFJt1OaZg"
+consumer_secret = "HJcVSeNyT8MxkTJWo3lNjRYCZMEvMe1rHuN0uxpXTskcLSaNnA"
+access_token = "266316265-qLKAe1864wDIaj8c5Qmeyji1LaIJvj9TfseCL1Bf"
+access_token_secret = "raHUN9Eha2E7T3koWDzQSMkMcvB4f3ZBkVqzvrwuAVvgr"
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
+
+#snap = API.update_status('Command line tweet'["Ann Arbor, USA"])
+
+#
+#try:
+	#x = open('tweet_cache_5.txt', 'r')
+	#cached_data = json.loads(x.read())
+	#print(type(cached_data))
+	#x.close()
+#except:
+	
+	#f = open('tweet_cache_5.txt', 'w')
+	#f.write(json.dumps(search_input))
+	#f.close()
+	#print(type(search_input)," is the type of publictweets
+
+	# start out cache
+CACHE_FNAME = "cached_data_socialmedia.json"
+try:
+	cache_file = open(CACHE_FNAME,'r')
+	cache_contents = cache_file.read()
+	CACHE_DICTION = json.loads(cache_contents)
+except:
+	CACHE_DICTION = {}
+
+def get_tweets_from_user(username): 
+	unique_identifier = "twitter_{}".format(username) # seestring formatting chapter
+	if unique_identifier in CACHE_DICTION: # if it is...
+		print('using cached data for', username)
+		twitter_results = CACHE_DICTION[unique_identifier] # grab the data from the cache!
+	else:
+		print('getting data from internet for', username)
+		twitter_results = api.search(username) # get it from the internet
+		# but also, save in the dictionary to cache it!
+		desired_tweets = twitter_results['statuses']			
+		CACHE_DICTION[unique_identifier] = twitter_results # add it to the dictionary -- new key-val pair
+		# and then write the whole cache dictionary, now with new info added, to the file, so it'll be there even after your program closes!
+		f = open(CACHE_FNAME,'w') # open the cache file for writing
+		f.write(json.dumps(CACHE_DICTION)) # make the whole dictionary holding data and unique identifiers into a json-formatted string, and write that wholllle string to a file so you'll have it next time!
+		f.close()
+
+	tweet_list = []
+	for x in tweet_list: 
+		tweet_list.append("Text: " + tweet['text'])
+		tweet_list.append("Created AT: " + tweet['created_at'])
+		tweet_list.append("\n")
+	return(tweet_list[:9])
+
+
+
+user_tweet = input("Watcha Tryna Search")
+tweet_user = get_tweets_from_user(user_tweet)
+for tweet in user_tweet:
+	print(tweet)
 
 ## TEXT: Go blue!
+
 ## CREATED AT: Sun Feb 12 12::35:19 +0000 2017
 
 ## .. plus one more.
@@ -35,14 +96,7 @@ import json
 ## **** If you choose not to do that, we strongly advise using authentication information for an 'extra' Twitter account you make just for this class, and not your personal account, because it's not ideal to share your authentication information for a real account that you use frequently.
 
 ## Get your secret values to authenticate to Twitter. You may replace each of these with variables rather than filling in the empty strings if you choose to do the secure way for 50 EC points
-consumer_key = "" 
-consumer_secret = ""
-access_token = ""
-access_token_secret = ""
-## Set up your authentication to Twitter
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth, parser=tweepy.parsers.JSONParser()) # Set up library to grab stuff from twitter with your authentication, and return it in a JSON-formatted way
+ # Set up library to grab stuff from twitter with your authentication, and return it in a JSON-formatted way
 
 ## Write the rest of your code here!
 
